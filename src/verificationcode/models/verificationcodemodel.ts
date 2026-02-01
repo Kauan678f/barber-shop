cc999dc2ec251c5a3b7365c979f52a3bbc004951
Kauan678f <kauancosta7898@gmail.com>
2026-02-01 20:27:33 -0300

    feat: verificationcodeservice:create

---
+++ src/verificationcode/models/verificationcodemodel.ts
@@ -0,0 +1,4 @@
+export interface VerificationCodeModel {
+    email: string,
+    code: string
+}
--- src/verificationcode/verificationcode.service.ts
+++ src/verificationcode/verificationcode.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { VerificationCodeModel } from './models/verificationCodeModel';
 
 @Injectable()
-export class VerificationcodeService {}
+export class VerificationcodeService {
+    constructor(private PrismaService: PrismaService){}
+
+    async create(data: VerificationCodeModel){
+        return await this.PrismaService.verificationCode.create({
+            data: data,
+            select: {
+                email: true
+            }
+        })
+    }
+}
